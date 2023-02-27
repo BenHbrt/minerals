04c35f6559661bd8d521275347ddf40b7d9b1421
BenHbrt <benherbert1983@googlemail.com>
2023-02-27 16:29:35 +0100

    Practice data and images

---
+++ src/data/mineralData.js
@@ -0,0 +1,30 @@
+export const minerals = [
+    {
+        name: "Aquamarine",
+        type: "Beryl",
+        image: "aquamarine.jpeg",
+        notes: "This is a pretty, shiny, blue, diamondy thing.",
+        link: "https://en.wikipedia.org/wiki/Aquamarine_(gem)"
+    },
+    {
+        name: "Emerald",
+        type: "Beryl",
+        image: "emerald.jpeg",
+        notes: "This is a pretty, shiny, green, diamondy thing.",
+        link: "https://en.wikipedia.org/wiki/Emerald"
+    },
+    {
+        name: "Amethyst",
+        type: "Silicate",
+        image: "amethyst.jpeg",
+        notes: "This is a pretty, shiny, purple, diamondy thing that comes in stoney egg shapes.",
+        link: "https://en.wikipedia.org/wiki/Aquamarine_(gem)"
+    },
+    {
+        name: "Citrine",
+        type: "Silicate",
+        image: "citrine.jpeg",
+        notes: "This is a pretty, shiny, yellowy-orange, diamondy thing.",
+        link: ""
+    }
+]
